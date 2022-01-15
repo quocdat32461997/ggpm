4d4069adc7f631f5a10a0a366d6654fd7b6d9c79
Eric Ngo <quocdat32461997@yahoo.com>
2022-01-15 00:38:08 -0600

    Added config-parsing to ease trainign and tracking procedures

---
+++ configs/configs.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+
+class Configs(object):
+    def __init__(self, path=None, args=None):
+        if path is not None:
+            assert path.endswith('.json') and os.path.exists(path) is True
+            self.from_json(open(path, 'r'))
+        elif args is not None:
+            assert isinstance(args, dict) is True
+            self.from_json(args)
+        else:
+            raise "Either path or args must be a valid value"
+
+    def to_json(self, path):
+        assert path.endswith('.json') and os.path.exists(path) is True
+        with  open(path, 'w') as file:
+            json.dumps(self.__dict__)
+
+    def from_json(self, json_str):
+        configs = json.load(json_str)
+        self.__dict__.update(configs)
+
