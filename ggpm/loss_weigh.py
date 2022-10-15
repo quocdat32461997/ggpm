0d36636e61d8ebfc8604fe1caf3106ac8ee69e00
Eric Ngo <quocdat32461997@yahoo.com>
2022-10-15 14:01:49 -0500

    Fixed individual-optimizer

--- ggpm/loss_weigh.py
+++ ggpm/loss_weigh.py
@@ -10,7 +10,7 @@ class LossWeigh(torch.nn.Module):
         self.recon_log_var = nn.Parameter(torch.zeros((1,), dtype=torch.float64), requires_grad=True)
 
     def compute_recon_loss(self, loss):
-        loss = loss * torch.exp(-self.recon_log_var) + self.recon_log_var/2
+        loss = loss * torch.exp(-self.recon_log_var) + self.recon_log_var
         return loss.sum()
 
     def compute_prop_loss(self, homo_loss, lumo_loss):
