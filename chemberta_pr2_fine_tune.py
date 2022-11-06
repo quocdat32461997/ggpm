057715e0f094004db954fa7a0bd1ff9ce21f4bb5
quocdat32461997 <quocdat32461997@yahoo.com>
2022-11-06 00:06:21 -0500

    Converted to multi-task regression

--- chemberta_pr2_fine_tune.py
+++ chemberta_pr2_fine_tune.py
@@ -16,7 +16,8 @@ from ggpm.nnutils import to_cuda
 # define command line argus
 parser = argparse.ArgumentParser()
 parser.add_argument('--path-to-config', required=True)
-parser.add_argument('--property-type', required=True)
+parser.add_argument('--homo-col', required=True)
+parser.add_argument('--lumo-col', required=True)
 args = parser.parse_args()
 
 MODEL_VERSION = 'seyonec/PubChem10M_SMILES_BPE_450k'
@@ -29,50 +30,76 @@ class ChemBertaForPR2(torch.nn.Module):
         self.backbone = RobertaModel.from_pretrained(MODEL_VERSION)
         embed_size = self.backbone.config.hidden_size
 
-        self.regressors = torch.nn.ModuleList()
+        self.homo_regressors = torch.nn.ModuleList()
+        self.lumo_regressors = torch.nn.ModuleList()
         for hidden_size in hidden_size_list:
-            self.regressors.extend([
+            self.homo_regressors.extend([
+                torch.nn.Linear(embed_size, hidden_size),
+                torch.nn.ReLU(),
+                torch.nn.Dropout(dropout),
+            ])
+            self.lumo_regressors.extend([
                 torch.nn.Linear(embed_size, hidden_size),
                 torch.nn.ReLU(),
                 torch.nn.Dropout(dropout),
             ])
             embed_size = hidden_size
-        self.regressors.append(torch.nn.Linear(hidden_size_list[-1], 1))
+        self.homo_regressors.append(torch.nn.Linear(hidden_size_list[-1], 1))
+        self.lumo_regressors.append(torch.nn.Linear(hidden_size_list[-1], 1))
 
         # loss
         self.mse_loss = torch.nn.MSELoss()
         self.mae_loss = torch.nn.L1Loss()
 
-    def forward(self, inputs, labels):
+    def forward(self, inputs, homo_labels, lumo_labels):
         # molecule embeddings
         outputs = self.backbone(inputs).pooler_output
-        for layer in self.regressors:
-            outputs = layer(outputs)
+
+        # regression
+        homo_outputs = self.homo_regressors[0](outputs)
+        lumo_outputs = self.lumo_regressors[0](outputs)
+        for homo_layer, lumo_layer in zip(self.homo_regressors[1:], self.lumo_regressors[1:]):
+            homo_outputs = homo_layer(homo_outputs)
+            lumo_outputs = lumo_layer(lumo_outputs)
+
 
         # flatten outputs
-        outputs = outputs.view(-1)
+        homo_outputs = homo_outputs.view(-1)
+        lumo_outputs = lumo_outputs.view(-1)
 
-        mae_loss = self.mae_loss(outputs, labels)
-        mse_loss = self.mse_loss(outputs, labels)
-        return {'mae': mae_loss, 'mse': mse_loss.item()}
+        # homo
+        homo_mae_loss = self.mae_loss(homo_outputs, homo_labels)
+        homo_mse_loss = self.mse_loss(homo_outputs, homo_labels)
+
+        # lumo
+        lumo_mae_loss = self.mae_loss(lumo_outputs, lumo_labels)
+        lumo_mse_loss = self.mse_loss(lumo_outputs, lumo_labels)
+
+        # total loss
+        loss = homo_mae_loss + lumo_mae_loss
+        return loss, {'homo_mae': homo_mae_loss.item(), 'homo_mse': homo_mse_loss.item(),
+                'lumo_mae': lumo_mae_loss.item(), 'lumo_mse': lumo_mse_loss.item()}
 
 
 class PR2Dataset(Dataset):
 
-    def __init__(self, path, smiles_col, property_col):
-        assert property_col in ['HOMO', 'LUMO']
+    def __init__(self, path, smiles_col, homo_col, lumo_col):
+        assert homo_col in ['HOMO', 'LUMO']
+        assert lumo_col in ['HOMO', 'LUMO']
 
         data = pd.read_csv(path)
         self.smiles = data[smiles_col]
-        self.property_labels = data[property_col]
+        self.homo_labels = data[homo_col]
+        self.lumo_labels = data[lumo_col]
 
     def __len__(self):
         return len(self.smiles)
 
     def __getitem__(self, index):
         smiles = self.smiles[index]
-        label = self.property_labels[index]
-        return smiles, label
+        homo_label = self.homo_labels[index]
+        lumo_label = self.lumo_labels[index]
+        return smiles, homo_label, lumo_label
 
 
 def fine_tune(args):
@@ -80,8 +107,8 @@ def fine_tune(args):
     configs = Configs(path=args.path_to_config)
 
     # get dataset
-    train_dataset = PR2Dataset(configs.data, smiles_col='SMILES', property_col=PROPERTY_MAP[args.property_type])
-    val_dataset = PR2Dataset(configs.val_data, smiles_col='SMILES', property_col=PROPERTY_MAP[args.property_type])
+    train_dataset = PR2Dataset(configs.data, smiles_col='SMILES', homo_col=PROPERTY_MAP[args.homo_col], lumo_col=PROPERTY_MAP[args.lumo_col])
+    val_dataset = PR2Dataset(configs.val_data, smiles_col='SMILES', homo_col=PROPERTY_MAP[args.homo_col], lumo_col = PROPERTY_MAP[args.lumo_col])
 
     # get tokenizer
     tokenizer = RobertaTokenizer.from_pretrained(MODEL_VERSION)
@@ -105,7 +132,7 @@ def fine_tune(args):
     metrics = defaultdict(float)
     for epoch in range(configs.epoch):
         train_dataloader = DataLoader(train_dataset, configs.batch_size)
-        for smiles, labels in train_dataloader:
+        for smiles, homo_labels, lumo_labels in train_dataloader:
             total_step += 1
             model.zero_grad()
             model.train()
@@ -113,15 +140,16 @@ def fine_tune(args):
             # encode data
             smiles_tokens = tokenizer(smiles, return_tensors='pt', add_special_tokens=True, padding=True)
             smiles_tokens = smiles_tokens['input_ids']
-            loss = model(to_cuda(smiles_tokens), to_cuda(labels))
+            loss, metrics_ = model(to_cuda(smiles_tokens), to_cuda(homo_labels), to_cuda(lumo_labels))
 
             # backprop
-            loss['mae'].backward()
+            loss.backward()
             optimizer.step()
 
-            # sum up metrics
-            metrics['mae'] += loss['mae'].item()
-            metrics['mse'] += loss['mse']
+            # accumulate metrics
+            for k, v in metrics_.items():
+                metrics[k] += v
+            metrics['loss'] += loss.item()
 
             if total_step % configs.print_iter == 0:
                 metrics = {k: v / configs.print_iter for k, v in metrics.items()}
@@ -141,13 +169,15 @@ def fine_tune(args):
                 val_metrics = defaultdict(float)
                 with torch.no_grad():
                     val_dataloader = DataLoader(val_dataset, configs.batch_size)
-                    for smiles, labels in val_dataloader:
+                    for smiles, homo_labels, lumo_labels in val_dataloader:
                         smiles_tokens = tokenizer(smiles, return_tensors='pt', add_special_tokens=True, padding=True)
                         smiles_tokens = smiles_tokens['input_ids']
-                        loss = model(to_cuda(smiles_tokens), to_cuda(labels))
+                        loss, metrics_ = model(to_cuda(smiles_tokens), to_cuda(homo_labels), to_cuda(lumo_labels))
 
-                        val_metrics['mae'] += loss['mae'].item()
-                        val_metrics['mse'] += loss['mse']
+                        # accumulate metrics
+                        for k, v in metrics_.items():
+                            val_metrics[k] += v 
+                        val_metrics['loss'] += loss.item()
 
                 # average val loss & metrics
                 n = len(val_dataloader)
@@ -160,12 +190,12 @@ def fine_tune(args):
                 del val_dataloader
                 # update early_stopping
                 if configs.early_stopping:
-                    early_stopping(val_metrics['mae'], model)
+                    early_stopping(val_metrics['loss'], model)
                     if early_stopping.early_stop:
                         break
-        del dataset
-        if args.save_iter == -1:
-            torch.save(model.state_dict(), args.save_dir + "/model." + str(epoch))
+        del train_dataloader
+        if configs.save_iter == -1:
+            torch.save(model.state_dict(), configs.save_dir + "/model." + str(epoch))
             scheduler.step()
             print("learning rate: %.6f" % scheduler.get_lr()[0])
 
