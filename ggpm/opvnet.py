ce525ca44fd41cdfe92dca7c9130ecdbebc25790
quocdat32461997 <quocdat32461997@yahoo.com>
2022-11-07 12:12:08 -0600

    Added single and two-split tasks for property-prediction

--- ggpm/opvnet.py
+++ ggpm/opvnet.py
@@ -1,11 +1,15 @@
 from ggpm.property_vae import *
+from evaluation.chemberta_pr2 import *
 
 class OPVNet:
     MODEL_DICT = {
             'prop': PropertyVAE,
             'prop-opt': PropOptVAE,
             'hier-prop': HierPropertyVAE,
-            'hier-prop-opt': HierPropOptVAE
+            'hier-prop-opt': HierPropOptVAE,
+            'pr2-single': ChemBertaForSinglePR2,
+            'pr2-two': ChemBertaForTwoPR2,
+            'pr2-two-split': ChemBertaForTwoSplitPR2,
             }
 
     @staticmethod
