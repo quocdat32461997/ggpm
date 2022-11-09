f34a969c3185c0a8a6a99b89cc17704436836b68
quocdat32461997 <quocdat32461997@yahoo.com>
2022-11-08 20:23:36 -0600

    Removed ChemBERTa for HOMO/LUMO prediction

--- ggpm/opvnet.py
+++ ggpm/opvnet.py
@@ -1,6 +1,5 @@
 from ggpm.property_vae import *
-import evaluation.property_nets.chemberta_pr2 as chemberta_pr2
-import evaluation.property_nets.schnet as schnet
+import evaluation.schnet as schnet
 
 class OPVNet:
     MODEL_DICT = {
@@ -8,9 +7,6 @@ class OPVNet:
             'prop-opt': PropOptVAE,
             'hier-prop': HierPropertyVAE,
             'hier-prop-opt': HierPropOptVAE,
-            'pr2-single': chemberta_pr2.ChemBertaForSinglePR2,
-            'pr2-two': chemberta_pr2.ChemBertaForTwoPR2,
-            'pr2-two-split': chemberta_pr2.ChemBertaForTwoSplitPR2,
             'schnet': schnet.SchNetwork
             }
 
